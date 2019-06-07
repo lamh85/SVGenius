ac4aacd35ded455a40f48c02fea875434005e465
Hon Lam <hon@junglescout.com>
2019-06-07 01:27:26 -0700

    WIP service test.

---
+++ test/model_generator/controller.test.js
@@ -0,0 +1,8 @@
+import { modelGenerate } from '../../src/model_generator/controller.js'
+import { generateDataPointStyles } from '../../src/model_generator/style_generator.js'
+
+describe('modelGenerate', () => {
+  it('passes arguments to generateDataPointStyles', () => {
+    
+  })
+})
